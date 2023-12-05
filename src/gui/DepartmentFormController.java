0864d7627a5ea89d5b387647ae1012f1c13bf893
moura <alisson_moura_rj@hotmail.com>
2023-12-05 11:58:08 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -72,6 +75,8 @@ public class DepartmentFormController implements Initializable {
 			notifyDataChangeListeners(); // atualiza a lista do department automaticamente.
 
 			Utils.currentStage(event).close();// fecha a janela(Enter Department data) de cadastro sozinha apos salvar.
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
 		} catch (DbException e) {
 			Alerts.showAlert("Error", null, e.getMessage(), AlertType.ERROR);
 		}
@@ -86,8 +91,23 @@ public class DepartmentFormController implements Initializable {
 	private Department getFormData() {
 		Department obj = new Department();
 
+		ValidationException exception = new ValidationException("Validadtion error"); // instanciando a exceção
+																						// personalizada.
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) { // .trim() elimina qualquer espaço em
+																				// branco digitado no final da digitção
+																				// do ususario.
+			exception.addError("name", "Field can't be empty");
+		}
 		obj.setName(txtName.getText());
+
+		if (exception.getErrors().size() > 0) { // testa se na coleção de erros tem pelo menos 1 erro. se verdadeiro,
+												// será lançado uma exceção.
+			throw exception;
+		}
+
 		return obj;
 	}
 
@@ -112,5 +132,13 @@ public class DepartmentFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 }
